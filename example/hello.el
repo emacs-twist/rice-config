e3e2e80030db21223475eb1db7ea1b47211ceead
Akira Komamura <akira.komamura@gmail.com>
2024-07-14 12:26:30 +0900

    Add hello.el

---
+++ example/hello.el
@@ -0,0 +1,35 @@
+;;; hello.el --- An example Emacs Lisp file -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1
+;; Package-Requires: ((emacs "28.1"))
+;; Keywords: maint
+;; URL: https://github.com/emacs-twist/rice-config
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is a fake library for use in tests.
+
+;;; Code:
+
+(provide 'hello)
+;;; hello.el ends here
