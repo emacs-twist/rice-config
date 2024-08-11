8aa348417abde8f9b8e15d734e6e44a67dec4378
Akira Komamura <akira.komamura@gmail.com>
2024-08-12 00:35:52 +0900

    style(template): Reformat

--- templates/default/flake.nix
+++ templates/default/flake.nix
@@ -1,16 +1,15 @@
 {
-  outputs = {...}: {
-    elisp-rice = {
-      packages =
-        builtins.trace
-        "[1;31mwarning: To use rice-config, please set the package name(s) in flake.nix[0m"
-        [
-          (abort "Please set the package name (without .el)")
-        ];
-      # Configure tests
-      tests = {
-        # buttercup.enable = true;
+  outputs =
+    { ... }:
+    {
+      elisp-rice = {
+        packages =
+          builtins.trace "[1;31mwarning: To use rice-config, please set the package name(s) in flake.nix[0m"
+            [ (abort "Please set the package name (without .el)") ];
+        # Configure tests
+        tests = {
+          # buttercup.enable = true;
+        };
       };
     };
-  };
 }
